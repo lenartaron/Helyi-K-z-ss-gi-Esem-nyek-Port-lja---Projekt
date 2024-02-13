a65cb2e05044c91de610a188c6fe1774040c4b73
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2024-02-13 09:30:10 +0100

    Alapok

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwPZjwBcr-Aov0RH_Ibd2rwbHGBB0HlPI",
+  authDomain: "helyi-kozossegi-esemenyek.firebaseapp.com",
+  projectId: "helyi-kozossegi-esemenyek",
+  storageBucket: "helyi-kozossegi-esemenyek.appspot.com",
+  messagingSenderId: "120678429448",
+  appId: "1:120678429448:web:b765c54d0696be13aa72bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
